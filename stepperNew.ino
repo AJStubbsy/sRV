198994e5aa318e9e9facdbd775ae05066634d343
AJStubbsy <adam.stubbs@gmail.com>
2021-01-27 11:13:17 +0000

    Add ota update to stepperNew

--- stepperNew.ino
+++ stepperNew.ino
@@ -21,6 +21,7 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <Stepper.h>
+#include <ArduinoOTA.h>
 
 const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
 // for your motor
@@ -112,6 +113,29 @@ void setup_wifi() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+ 
+  ArduinoOTA.setHostname("ESP8266");
+  ArduinoOTA.setPassword("esp8266");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("OTA ready");
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -195,7 +219,7 @@ void reconnect() {
   }
 }
 void loop() {
-
+  ArduinoOTA.handle();
   if (!client.connected()) {
     reconnect();
   }
